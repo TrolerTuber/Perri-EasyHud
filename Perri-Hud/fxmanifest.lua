edf562710144c156af510111c31173132f9a12e3
TrolerTuber <94766847+TrolerTuber@users.noreply.github.com>
2024-05-22 18:16:39 +0200

    Add files via upload

---
+++ Perri-Hud/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+author "PerriTuber"
+description "https://discord.gg/HCaZt8PzbM"
+game "gta5"
+
+lua54 'yes'
+
+client_scripts {
+    'client/*.lua',
+}
+
+
+ui_page ("ui/index.html")
+
+files {'**/**/**/**/**/**/*.*'}
